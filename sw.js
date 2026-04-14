aca930848976420c947a55980124b524c86ce66b
karlokinawa <landoltkarl@gmail.com>
2026-04-14 12:14:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,64 +1,68 @@
-const CACHE_NAME = 'mega4-pwa-v11-true-light';
+const CACHE_NAME = 'mega4-pwa-v42';
 const APP_FILES = [
-  "a01.m4a",
-  "a02.m4a",
-  "a03.m4a",
-  "a04.m4a",
-  "a05.m4a",
-  "a06.m4a",
-  "a07.m4a",
-  "ch_hour.png",
-  "chrono_min.png",
-  "clickHeavy.m4a",
-  "clickSoft.m4a",
-  "eq_time.png",
-  "file_list.txt",
-  "gmt_l.png",
-  "gmt_l_sub.png",
-  "gmt_r.png",
-  "gmt_r_sub.png",
-  "h24.png",
-  "i01.png",
-  "i02.png",
-  "i03.png",
-  "i04.png",
-  "i06.png",
-  "i07.png",
-  "i08.png",
-  "i09.png",
-  "i10.png",
-  "i13.png",
-  "i14.png",
-  "i15.png",
-  "i16.png",
-  "i17.png",
-  "i18.png",
-  "i19.png",
-  "i20.png",
-  "index.html",
-  "leap.png",
-  "manifest.webmanifest",
-  "overlay_arrow.png",
-  "overlay_de.png",
-  "overlay_glitter.png",
-  "overlay_info.png",
-  "overlay_jp.png",
-  "p01.png",
-  "p02.webp",
-  "p03.webp",
-  "p04.webp",
-  "p05.webp",
-  "sw.js",
-  "u09.html"
+  "./K1_btn.png",
+  "./K2_btn.png",
+  "./K3_btn.png",
+  "./K4_btn.png",
+  "./K5_btn.png",
+  "./K6_btn.png",
+  "./K7_btn.png",
+  "./L13.png",
+  "./L14.png",
+  "./L15.png",
+  "./L16.png",
+  "./L17.png",
+  "./L18.png",
+  "./L20.png",
+  "./L21.png",
+  "./L21B.png",
+  "./L22.png",
+  "./L26.png",
+  "./L27.png",
+  "./audio01.m4a",
+  "./audio02.m4a",
+  "./audio03.m4a",
+  "./audio04.m4a",
+  "./audio05.m4a",
+  "./audio06.m4a",
+  "./audio07.m4a",
+  "./audio08.m4a",
+  "./audio09.m4a",
+  "./balance.png",
+  "./bg.png",
+  "./dial.png",
+  "./gmt_l.png",
+  "./gmt_r.png",
+  "./h24.png",
+  "./hourHand.png",
+  "./img01.png",
+  "./img02.png",
+  "./img03.png",
+  "./index.html",
+  "./infoOverlay.png",
+  "./leap.png",
+  "./m_light.html",
+  "./manifest.webmanifest",
+  "./minuteHand.png",
+  "./moon.png",
+  "./pinion1.png",
+  "./pinion2.png",
+  "./sec_wheel2.png",
+  "./tourb_cage.png"
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)).then(() => self.skipWaiting()));
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+  );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim())
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
@@ -66,10 +70,10 @@ self.addEventListener('fetch', event => {
   const req = event.request;
   if (req.method !== 'GET') return;
   event.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).then(resp => {
+    fetch(req).then(resp => {
       const copy = resp.clone();
       caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
       return resp;
-    }).catch(() => cached))
+    }).catch(() => caches.match(req))
   );
 });
